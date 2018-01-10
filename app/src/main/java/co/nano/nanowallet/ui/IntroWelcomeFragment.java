7ec45a7625e6c289afd26a08a8cf10467cf16861
Steve Zeidner <szeidner@gmail.com>
2018-01-10 09:20:56 -0500

    Add databinding.

--- app/src/main/java/co/nano/nanowallet/ui/IntroWelcomeFragment.java
+++ app/src/main/java/co/nano/nanowallet/ui/IntroWelcomeFragment.java
@@ -1,43 +1,33 @@
 package co.nano.nanowallet.ui;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 import co.nano.nanowallet.BuildConfig;
 import co.nano.nanowallet.R;
+import co.nano.nanowallet.databinding.FragmentIntroWelcomeBinding;
 
 /**
  * The Intro Screen to the app
  */
 
 public class IntroWelcomeFragment extends Fragment {
-    @BindView(R.id.intro_welcome_version)
-    TextView version;
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_intro_welcome, container, false);
+        // inflate the view
+        FragmentIntroWelcomeBinding binding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_intro_welcome, container, false);
+        View view = binding.getRoot();
 
-        // bind views
-        ButterKnife.bind(this, v);
-
-        // bind data to views
-        bindData();
-
-        return v;
-    }
-
-    private void bindData() {
-        version.setText(getString(R.string.version_display, BuildConfig.VERSION_NAME));
+        // bind data to view
+        binding.setVersion(getString(R.string.version_display, BuildConfig.VERSION_NAME));
+        
+        return view;
     }
 
 
