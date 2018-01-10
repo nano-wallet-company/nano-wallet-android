a387b5f9b3a4261063dd280a66a47ea651f9d832
Steve Zeidner <szeidner@gmail.com>
2018-01-10 10:06:54 -0500

    Add click bindings for buttons.

--- app/src/main/java/co/nano/nanowallet/ui/IntroWelcomeFragment.java
+++ app/src/main/java/co/nano/nanowallet/ui/IntroWelcomeFragment.java
@@ -3,6 +3,7 @@ package co.nano.nanowallet.ui;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +17,9 @@ import co.nano.nanowallet.databinding.FragmentIntroWelcomeBinding;
  */
 
 public class IntroWelcomeFragment extends Fragment {
+    public String TAG = IntroWelcomeFragment.class.getSimpleName();
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -26,8 +30,20 @@ public class IntroWelcomeFragment extends Fragment {
 
         // bind data to view
         binding.setVersion(getString(R.string.version_display, BuildConfig.VERSION_NAME));
-        
+        binding.setHandlers(new ClickHandlers());
+
         return view;
+    }
+
+
+    public class ClickHandlers {
+        public void onClickNewWallet(View view) {
+            Log.d(TAG, "New Wallet");
+        }
+
+        public void onClickHaveWallet(View view) {
+            Log.d(TAG, "Have Wallet");
+        }
     }
 
 
