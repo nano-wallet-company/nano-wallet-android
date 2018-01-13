8351abda89674ad0d6868f3cf3534f1c582df3be
Steve Zeidner <szeidner@gmail.com>
2018-01-13 15:49:10 -0500

    Add swipeable wallet amounts.

--- app/src/main/java/co/nano/nanowallet/ui/home/HomePagerEnum.java
+++ app/src/main/java/co/nano/nanowallet/ui/home/HomePagerEnum.java
@@ -7,9 +7,9 @@ import co.nano.nanowallet.R;
  */
 
 public enum HomePagerEnum {
-    NANO(R.layout.view_home_wallet),
-    USD(R.layout.view_home_wallet),
-    BTC(R.layout.view_home_wallet);
+    NANO(R.layout.view_home_amount_nano),
+    BTC(R.layout.view_home_amount_btc),
+    USD(R.layout.view_home_amount_usd);
 
     private int mLayoutResId;
 
