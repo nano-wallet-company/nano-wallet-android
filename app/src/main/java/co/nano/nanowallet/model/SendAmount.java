7f6501261be458c7401b0ad83124fa829d6e0726
Steve Zeidner <szeidner@gmail.com>
2018-01-28 17:50:39 -0500

    Fix decimal point issue with send amount.

--- app/src/main/java/co/nano/nanowallet/model/SendAmount.java
+++ app/src/main/java/co/nano/nanowallet/model/SendAmount.java
@@ -7,43 +7,84 @@ import java.util.Currency;
 import java.util.Locale;
 
 /**
- * Sending amount to another user
+ * Holds display amounts to send to another user
  */
 
 public class SendAmount {
     private String nanoAmount;
     private String localCurrencyAmount;
 
+    /**
+     * Default Constructor
+     */
     public SendAmount() {
         nanoAmount = "";
         localCurrencyAmount = "";
     }
 
+    /**
+     * Set String representations of nanoAmount and localCurrencyAmount
+     * @param nanoAmount
+     * @param localCurrencyAmount
+     */
     public SendAmount(String nanoAmount, String localCurrencyAmount) {
         this.nanoAmount = nanoAmount;
         this.localCurrencyAmount = localCurrencyAmount;
     }
 
+    /**
+     * Get Nano amount entered
+     * @return String
+     */
     public String getNanoAmount() {
         return nanoAmount;
     }
 
+
+    /**
+     * Set Nano amount which will also set the local currency amount
+     * @param nanoAmount String Nano Amount from input
+     */
     public void setNanoAmount(String nanoAmount) {
         this.nanoAmount = nanoAmount;
         if (nanoAmount.length() > 0) {
-            this.localCurrencyAmount =
-                    new BigDecimal(nanoAmount)
-                            .multiply(new BigDecimal(30))
-                            .toString();
+            if (nanoAmount.equals(".")) {
+                this.nanoAmount = "0.";
+            }
+            this.localCurrencyAmount = convertNanoToLocalCurrency(this.nanoAmount);
         } else {
             this.localCurrencyAmount = "";
         }
     }
 
+    /**
+     * Convert a Nano string amount to a local currency String
+     * @param amount String amount of Nano
+     * @return String local currency amount
+     */
+    private String convertNanoToLocalCurrency(String amount) {
+        if (amount.equals("0.")) {
+            return amount;
+        } else {
+            // TODO: Add real conversion
+            return new BigDecimal(amount)
+                    .multiply(new BigDecimal(30))
+                    .toString();
+        }
+    }
+
+    /**
+     * Return the  local currency amount as a string
+     * @return
+     */
     public String getLocalCurrencyAmount() {
         return localCurrencyAmount;
     }
 
+    /**
+     * Return the currency formatted local currency amount as a string
+     * @return
+     */
     public String getLocalCurrencyAmountFormatted() {
         if (localCurrencyAmount.length() > 0) {
             return currencyFormat(Float.valueOf(localCurrencyAmount));
@@ -52,17 +93,41 @@ public class SendAmount {
         }
     }
 
+    /**
+     * Set the local currency amount and also update the nano amount to match
+     * @param localCurrencyAmount String of local currency amount from input
+     */
     public void setLocalCurrencyAmount(String localCurrencyAmount) {
         this.localCurrencyAmount = localCurrencyAmount;
         if (localCurrencyAmount.length() > 0) {
-            this.nanoAmount = new BigDecimal(localCurrencyAmount)
-                    .divide(new BigDecimal(30), RoundingMode.FLOOR)
-                    .toString();
+            if (localCurrencyAmount.equals(".")) {
+                this.localCurrencyAmount = "0.";
+            }
+            this.nanoAmount = convertLocalCurrencyToNano(this.localCurrencyAmount);
         } else {
             this.nanoAmount = "";
         }
     }
 
+    /**
+     * Convert a local currency string to a Nano string
+     * @param amount Local currency amount from input
+     * @return String of Nano converted amount
+     */
+    private String convertLocalCurrencyToNano(String amount) {
+        if (amount.equals("0.")) {
+            return amount;
+        } else {
+            // TODO: implement actual conversion
+            return new BigDecimal(amount)
+                    .divide(new BigDecimal(30), RoundingMode.FLOOR)
+                    .toString();
+        }
+    }
+
+    /**
+     * Convert local currency to properly formatted string for the currency
+     */
     private String currencyFormat(float amount) {
         NumberFormat format = NumberFormat.getCurrencyInstance(Locale.getDefault());
         format.setCurrency(Currency.getInstance("USD")); // TODO: get saved local currency
