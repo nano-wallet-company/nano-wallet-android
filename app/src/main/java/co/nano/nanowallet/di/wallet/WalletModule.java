e4a6c5042bd9b74e274680ccf583cc4b45af56ea
Steve Zeidner <szeidner@gmail.com>
2018-02-16 12:44:08 -0500

    Make wallet a singleton.

--- app/src/main/java/co/nano/nanowallet/di/wallet/WalletModule.java
+++ app/src/main/java/co/nano/nanowallet/di/wallet/WalletModule.java
@@ -2,6 +2,7 @@ package co.nano.nanowallet.di.wallet;
 
 import android.content.Context;
 
+import co.nano.nanowallet.di.activity.ActivityScope;
 import co.nano.nanowallet.model.NanoWallet;
 import dagger.Module;
 import dagger.Provides;
@@ -9,6 +10,7 @@ import dagger.Provides;
 @Module
 public class WalletModule {
     @Provides
+    @ActivityScope
     NanoWallet providesNanoWallet(Context context) {
         return new NanoWallet(context);
     }
