c8ecd819c62b1bc40e45c77535a68571aee2f4d4
Steve Zeidner <szeidner@gmail.com>
2018-03-07 11:11:04 -0500

    Add null checks to context in color utilities.

--- app/src/main/java/co/nano/nanowallet/ui/common/UIUtil.java
+++ app/src/main/java/co/nano/nanowallet/ui/common/UIUtil.java
@@ -2,17 +2,11 @@ package co.nano.nanowallet.ui.common;
 
 import android.content.Context;
 import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Matrix;
 import android.support.v4.content.ContextCompat;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
 import android.util.DisplayMetrics;
-import android.view.View;
-
-import java.io.ByteArrayOutputStream;
 
 import co.nano.nanowallet.R;
 
@@ -30,6 +24,9 @@ public class UIUtil {
      * @param context
      */
     public static void colorizeSpannable(Spannable s, Context context) {
+        if (context == null) {
+            return;
+        }
         if (s.length() > 0) {
             s.setSpan(new ForegroundColorSpan(ContextCompat.getColor(context, R.color.dark_sky_blue)), 0, s.length() > 8 ? 9 : s.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
             if (s.length() > 59) {
@@ -47,6 +44,9 @@ public class UIUtil {
      * @param context
      */
     public static void colorizeSeed(Spannable s, Context context) {
+        if (context == null) {
+            return;
+        }
         s.setSpan(new ForegroundColorSpan(ContextCompat.getColor(context, R.color.bright_white)), 0, s.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         s.setSpan(new ForegroundColorSpan(ContextCompat.getColor(context, R.color.dark_sky_blue)), 0, s.length() > 4 ? 5 : s.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         if (s.length() > 59) {
@@ -96,41 +96,5 @@ public class UIUtil {
         DisplayMetrics metrics = resources.getDisplayMetrics();
         float dp = px / ((float) metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
         return dp;
-    }
-
-    /**
-     * Convert a view into a byte array
-     *
-     * @param view Inflated view
-     * @return byte[]
-     */
-    public static byte[] viewToByteArray(View view) {
-        // create bitmap from view
-        Bitmap bitmap = Bitmap.createBitmap(200, 200, Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bitmap);
-        view.draw(canvas);
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, bytes);
-        return bytes.toByteArray();
-    }
-
-    /**
-     * Merge Bitmaps
-     * @param overlay top bitmap
-     * @param bitmap bottom bitmap
-     * @return
-     */
-    public static Bitmap mergeBitmaps(Bitmap overlay, Bitmap bitmap) {
-        int height = bitmap.getHeight();
-        int width = bitmap.getWidth();
-        Bitmap combined = Bitmap.createBitmap(width, height, bitmap.getConfig());
-        Canvas canvas = new Canvas(combined);
-        int canvasWidth = canvas.getWidth();
-        int canvasHeight = canvas.getHeight();
-        canvas.drawBitmap(bitmap, new Matrix(), null);
-        int centreX = (canvasWidth - overlay.getWidth()) / 2;
-        int centreY = (canvasHeight - overlay.getHeight()) / 2;
-        canvas.drawBitmap(overlay, centreX, centreY, null);
-        return combined;
     }
 }
