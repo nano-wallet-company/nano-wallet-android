4df813068c82029069cf777d359af5199f8861e4
Steve Zeidner <szeidner@gmail.com>
2018-04-02 12:11:28 -0400

    Add fix for null encryption key.

--- app/src/main/java/co/nano/nanowallet/di/persistence/PersistenceModule.java
+++ app/src/main/java/co/nano/nanowallet/di/persistence/PersistenceModule.java
@@ -30,69 +30,36 @@ public class PersistenceModule {
 
     @Provides
     Realm providesRealmInstance(@Named("encryption_key") byte[] key) {
-        if (key != null) {
-            try {
-                RealmConfiguration realmConfiguration = new RealmConfiguration.Builder()
-                        .name(DB_NAME)
-                        .encryptionKey(key)
-                        .schemaVersion(SCHEMA_VERSION)
-                        .migration(new Migration())
-                        .build();
+        try {
+            RealmConfiguration realmConfiguration = new RealmConfiguration.Builder()
+                    .name(DB_NAME)
+                    .encryptionKey(key)
+                    .schemaVersion(SCHEMA_VERSION)
+                    .migration(new Migration())
+                    .build();
 
-                // Open the Realm with encryption enabled
-                return Realm.getInstance(realmConfiguration);
-            } catch (RealmFileException e) {
-                // regenerate key and open realm with new key
-                Vault.getVault()
-                        .edit()
-                        .putString(Vault.ENCRYPTION_KEY_NAME,
-                                Base64.encodeToString(Vault.generateKey(), Base64.DEFAULT))
-                        .apply();
+            // Open the Realm with encryption enabled
+            return Realm.getInstance(realmConfiguration);
+        } catch (RealmFileException e) {
+            // regenerate key and open realm with new key
+            Vault.getVault()
+                    .edit()
+                    .putString(Vault.ENCRYPTION_KEY_NAME,
+                            Base64.encodeToString(Vault.generateKey(), Base64.DEFAULT))
+                    .apply();
 
-                RealmConfiguration realmConfiguration = new RealmConfiguration.Builder()
-                        .name(DB_NAME)
-                        .encryptionKey(Base64.decode(Vault.getVault().getString(Vault.ENCRYPTION_KEY_NAME, null), Base64.DEFAULT))
-                        .schemaVersion(SCHEMA_VERSION)
-                        .migration(new Migration())
-                        .build();
+            RealmConfiguration realmConfiguration = new RealmConfiguration.Builder()
+                    .name(DB_NAME)
+                    .encryptionKey(Base64.decode(Vault.getVault().getString(Vault.ENCRYPTION_KEY_NAME, null), Base64.DEFAULT))
+                    .schemaVersion(SCHEMA_VERSION)
+                    .migration(new Migration())
+                    .build();
 
-                Realm.deleteRealm(realmConfiguration);
+            Realm.deleteRealm(realmConfiguration);
 
-                RxBus.get().post(new Logout());
+            RxBus.get().post(new Logout());
 
-                return Realm.getInstance(realmConfiguration);
-            }
-        } else {
-            try {
-                RealmConfiguration realmConfiguration = new RealmConfiguration.Builder()
-                        .name(DB_NAME)
-                        .schemaVersion(SCHEMA_VERSION)
-                        .migration(new Migration())
-                        .build();
-
-                // Open the Realm with encryption enabled
-                return Realm.getInstance(realmConfiguration);
-            } catch (RealmFileException e) {
-                // regenerate key and open realm with new key
-                Vault.getVault()
-                        .edit()
-                        .putString(Vault.ENCRYPTION_KEY_NAME,
-                                Base64.encodeToString(Vault.generateKey(), Base64.DEFAULT))
-                        .apply();
-
-                RealmConfiguration realmConfiguration = new RealmConfiguration.Builder()
-                        .name(DB_NAME)
-                        .encryptionKey(Base64.decode(Vault.getVault().getString(Vault.ENCRYPTION_KEY_NAME, null), Base64.DEFAULT))
-                        .schemaVersion(SCHEMA_VERSION)
-                        .migration(new Migration())
-                        .build();
-
-                Realm.deleteRealm(realmConfiguration);
-
-                RxBus.get().post(new Logout());
-
-                return Realm.getInstance(realmConfiguration);
-            }
+            return Realm.getInstance(realmConfiguration);
         }
     }
 
@@ -116,7 +83,7 @@ public class PersistenceModule {
         if (Vault.getVault() != null && Vault.getVault().getString(Vault.ENCRYPTION_KEY_NAME, null) != null) {
             return Base64.decode(Vault.getVault().getString(Vault.ENCRYPTION_KEY_NAME, null), Base64.DEFAULT);
         } else {
-            return null;
+            return Vault.generateKey();
         }
     }
 }
