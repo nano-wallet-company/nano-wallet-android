8e0e565013f71452fb2f28651f3326baa3db6655
Steve Zeidner <szeidner@gmail.com>
2018-06-22 09:06:47 -0400

    Remove incorrectly placed executeSend()

--- app/src/main/java/co/nano/nanowallet/ui/send/SendFragment.java
+++ app/src/main/java/co/nano/nanowallet/ui/send/SendFragment.java
@@ -312,7 +312,6 @@ public class SendFragment extends BaseFragment {
                 credentials.setNewlyGeneratedSeed(newSeed);
             }
             realm.commitTransaction();
-            executeSend();
             AlertDialog.Builder builder;
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 builder = new AlertDialog.Builder(getContext(), android.R.style.Theme_Material_Light_Dialog_Alert);
